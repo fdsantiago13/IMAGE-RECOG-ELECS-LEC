107ea0637a6e51bcacdb99e700c3bf45e3386a09
Fern Santigo <fdsantiago2020@plm.edu.ph>
2023-08-10 22:40:24 +0800

    test

--- IMAGERECOG.py
+++ IMAGERECOG.py
@@ -3,17 +3,15 @@ import numpy as np
 import face_recognition
 import os
 
-path = './FINAL ACT IMAGE LEC 2ND TRY/DATASET'
+path = './ELECS-LEC-WEBCAM/DATASET'
 images = []
 classNames = []
 myList = os.listdir(path)
-
 
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
-print(classNames)
 
 
 def findEncodings(images):
@@ -27,7 +25,7 @@ def findEncodings(images):
 
 
 encodeListKnown = findEncodings(images)
-print('Encoding Complete')
+
 
 cap = cv2.VideoCapture(0)
 
